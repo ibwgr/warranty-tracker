143e634663f06645e242e9e14ec1e886a83a5b79
Hubbacca <54737443+Hubbacca@users.noreply.github.com>
2020-12-22 15:30:22 +0100

    Correction for dependencies & test-route

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/test-route');
+const testRouter = require('./routes/test-route');
 
 const app = express();
 
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public', 'index.html')));
 
 app.use('/', indexRouter);
-app.use('/test-route', usersRouter);
+app.use('/test-route', testRouter);
 
 const port = 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
