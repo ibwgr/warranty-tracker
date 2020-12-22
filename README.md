3a0df32cd545f71ce3d21838bfd3216edc293532
Hubbacca <54737443+Hubbacca@users.noreply.github.com>
2020-12-22 15:11:36 +0100

    general project setup - initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+### Installation
+xXx
+### Module
+xXx
+### Usage
+xXx
+### Unit tests
+xXx
+### General information
+xXx
+
+
---
+++ app/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/test-route');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../public', 'index.html')));
+
+app.use('/', indexRouter);
+app.use('/test-route', usersRouter);
+
+const port = 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+module.exports = app;
