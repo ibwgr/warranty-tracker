5871cb9e2ce1389722631fa4c97847620150cd99
ladral <tuor_philipp@hotmail.com>
2020-12-23 17:28:42 +0100

    implementation of simple navigation bar

--- app/app.js
+++ app/app.js
@@ -6,6 +6,8 @@ const testRouter = require('./routes/test-route');
 
 const app = express();
 
+app.use(express.static('public'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public', 'index.html')));
