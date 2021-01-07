b6384b4653b4e8bb7ae9f9b8b894bbfc69c02c34
ladral <tuor_philipp@hotmail.com>
2021-01-07 19:51:35 +0100

    connect to database without password and on default port

--- app/model/database.js
+++ app/model/database.js
@@ -1,8 +1,8 @@
 const mariadb = require('mariadb');
 const pool = mariadb.createPool({
     user: "root",
-    password: "ibwsa3",
-    port: 3333,
+    //password: "ibwsa3",
+    port: 3306,
     database: "warrantydb",
     connectionLimit: 5
 });
